cd73641a7dce927012f01f67d32d1bded791654c
Dariusmumbere <dariusmumbere@gmail.com>
2025-07-11 12:54:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm import sessionmaker, Session, relationship
 from sqlalchemy.sql import func
 
 # Database configuration
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://username:password@localhost/reporting_system")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/inventory")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
